f2362e9add7f1d683c222803ebf1de53d807c276
Dreykolin7 <dparedes2020@inf.udec.cl>
2023-06-27 23:20:13 -0400

    main2 final

--- main2.cpp
+++ main2.cpp
@@ -1,17 +1,18 @@
 /*#include "raiz.h"
-
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <string>
 #include <stack>
-#include <algorithm>
+#include <algorithm>´
+#include <chrono>
+#include <cstdlib>}
 #define Archivo "worldcitiespop_fixed.csv"
 int main() {
     double latmax = -200;
     double latmin = 200;
 
-    double lonmax = -200;
+    double lonmax = -200; //inicializamos las capacidades minimas y maximas con valores que si o si van a superar.
     double lonmin = 200;
 
    
@@ -25,7 +26,7 @@ int main() {
     getline(archivo, linea);
     // Leemos todas las líneas
     int j = 0;
-    while (getline(archivo, linea)  && j < 20)
+    while (getline(archivo, linea)  && j < 3200000)
     {
         j++;
         stringstream stream(linea); // Convertir la cadena a un stream
@@ -45,8 +46,8 @@ int main() {
         std::replace(Longitude.begin(), Longitude.end(), ',', '.');
 
 
-        double num1_double = std::stod(Latitude);
-        double num2_double = std::stod(Longitude);  // se convierten
+        double num1_double = std::stod(Latitude) ;
+        double num2_double = std::stod(Longitude) ;  // se convierten
         int num3_int = std::stoi(Population);
         Node* nodo = new Node(Point(num1_double, num2_double), num3_int); //creamos un  nodo, es necesario para almacenarlos en un stack
 
@@ -64,36 +65,83 @@ int main() {
         }
 
         stacknodo.push(nodo);  // se almacena
-     /*   cout << "==================" << endl;
-        cout << "Latitude nodo: " << nodo->pos.x << endl;  //para comprobar
-        cout << "Longitude nodo:" << nodo->pos.y << endl;
-        cout << "Pop nodo: " << nodo->data << endl;
-
-        
+    // podria añadir directamente los puntos, pero no podríamos tener latmin,lonmin,latmax,lonmax. Además, calcular cuanto tarda en insertarse
+                      //es más directo de esta forma.
     }
     archivo.close();
-    QuadArbol plano(Point(latmin, lonmin), Point(latmax, lonmax));
+    QuadArbol plano(Point(latmin, lonmin), Point(latmax, lonmax)); //el arbol se crea en base a los datos más extremos
+    cout << "latmax: " << latmax << endl;
+        cout << "latmin: " << latmin << endl;
+        cout << "lonmax: " << lonmax << endl;
+        cout << "lonmin: " << lonmin << endl;
     for (int i = 0; i < j; i++) {
-        //   cout << "nodossssss latitudee" << stacknodo.top()->pos.x << endl;  //esto es solo para ver que quedaron correctamente en el stack
+     //    cout << "nodossssss latitudee" << stacknodo.top()->pos.x << endl;  //esto es solo para ver que quedaron correctamente en el stack
         plano.insert(stacknodo.top()->pos, stacknodo.top()->data);
-
-
-        Node* p = plano.search(stacknodo.top()->pos);
-        if (p != NULL) cout << p->data << endl;   // comprobaremos que se hayan insertado
-        else cout << "No existe el punto." << endl; // en el ejemplo que doy, hay uno que no se introdujo, debido a que su longitud sobrepasa los 100
-
-        stacknodo.pop();
-    }
-    cout << "Agreggate region "<<plano.AggregateRegion(Point((latmax+latmin)/2,(lonmax+lonmin)/2),400)<<endl;
-
-
-
-
     
+     // Node* p = plano.search(stacknodo.top()->pos);
+       // if (p != NULL) cout << p->data << endl;   // comprobaremos que se hayan insertado
+       // else cout << "No existe el punto." << endl; 
+       
+stacknodo.pop();
+    }
 
+
+
+    //Preorder para recorrer
+    plano.preorder(plano.getraiz());
+    cout << endl;
+    //solo toma los nodos negros
+    cout << "Cantidad de nodos: " << plano.totalNodes() << endl;
+    cout << endl;
+
+
+
+
+    cout << "Cantidad de puntos: " << plano.countRegion(Point(latmin, lonmin), 360) << endl; //ejemplo en canvas
+
+
+
+
+    auto start = chrono::steady_clock::now();
+    for (int i = 0; i < 20; i++) {
+        cout << "Poblacion estimada: " << plano.AggregateRegion(Point(latmin, lonmin), 360) << endl; //ejemplo en canvas
+    }
+    auto end = chrono::steady_clock::now();  //esto usamos para calcular el tiempo
+
+
+
+    cout << "Elapsed time in nanoseconds: "
+        << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
+        << " ns" << endl;
+
+    cout << "Elapsed time in microseconds: "
+        << chrono::duration_cast<chrono::microseconds>(end - start).count()
+        << " µs" << endl;
+
+    cout << "Elapsed time in milliseconds: "
+        << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+        << " ms" << endl;
+
+    cout << "Elapsed time in seconds: "
+        << chrono::duration_cast<chrono::seconds>(end - start).count()
+        << " sec";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
 }
-
-
 */
 
 
